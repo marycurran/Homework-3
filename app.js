ed8032080b7ec9fd1629b3711209479a89318305
marycurran <mary.curran@student.csulb.edu>
2019-09-30 19:53:30 -0700

    commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const org1_depts=[
+{
+    name:'accounting',
+    children: [
+        {name: 'accounting payable', children: []},
+        {name: 'accounting recievable', children []},
+        ],
+    },
+    {
+    name: 'finance',
+    children: [],
+    }
+]
